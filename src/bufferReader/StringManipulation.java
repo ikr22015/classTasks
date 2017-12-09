9c8d8561feab033ba95b9805c595449b7baddc11
Imran Kabir <ikr22015@gmail.com>
2017-12-08 20:02:06 -0500

    Added charAt() method

--- src/bufferReader/StringManipulation.java
+++ src/bufferReader/StringManipulation.java
@@ -3,8 +3,13 @@ package bufferReader;
 public class StringManipulation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/* Method-1: charAt().
+		 * Description: Returns the character at the specified index.
+		 * Return Value: This method returns a char at the specified index.
+		 * */
+		String s = "Strings are immutable";
+		char result = s.charAt(9);
+		System.out.println(result);
 	}
 
 }
